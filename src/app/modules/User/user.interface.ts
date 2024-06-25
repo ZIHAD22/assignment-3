9d3de50184650f24e81cc50657653402ca62baf8
ZIHAD22 <mdzihad2244@gmail.com>
2024-06-25 23:06:27 +0600

    add user interface and model

---
+++ src/app/modules/User/user.interface.ts
@@ -0,0 +1,10 @@
+type TUser = {
+  name: string;
+  email: string;
+  password: string;
+  phone: string;
+  role: "admin" | "user";
+  address: string;
+};
+
+export default TUser;
---
+++ src/app/modules/User/user.model.ts
@@ -0,0 +1,34 @@
+import { Schema, model } from "mongoose";
+import TUser from "./user.interface";
+
+const userSchema = new Schema<TUser>({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ["admin", "user"],
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+});
+
+const UserModel = model<TUser>("User", userSchema);
+
+export default UserModel;
