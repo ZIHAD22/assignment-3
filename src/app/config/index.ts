a1279a4fb84a59ffdc718ccbafa2c621cb871a61
ZIHAD22 <mdzihad2244@gmail.com>
2024-06-26 15:12:27 +0600

    add dev url and prod url for database

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -2,8 +2,19 @@ import dotEnv from "dotenv";
 
 dotEnv.config();
 
+// export env variable
 const PORT = process.env.PORT;
-const PROD_DB_URL = process.env.PROD_DB_URL;
-const DEV_DB_URL = process.env.DEV_DB_URL;
+const PROD_DB_URL = process.env.PROD_DB_URL as string;
+const DEV_DB_URL = process.env.DEV_DB_URL as string;
 
-export { PORT, PROD_DB_URL, DEV_DB_URL };
+// node env variable for database url
+const NODE_ENV = process.env.NODE_ENV;
+let prod_url: string;
+
+if (NODE_ENV === "DEV") {
+  prod_url = DEV_DB_URL;
+} else if (NODE_ENV === "PROD") {
+  prod_url = PROD_DB_URL;
+}
+
+export { PORT, prod_url };
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
 import mongoose, { connect } from "mongoose";
 import app from "./app";
-import { PORT, PROD_DB_URL } from "./app/config";
+import { PORT, prod_url } from "./app/config";
 
 const main = async () => {
-  await connect(PROD_DB_URL as string);
+  await connect(prod_url);
   app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
     console.log("Database Connected");
