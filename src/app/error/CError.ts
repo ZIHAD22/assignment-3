8edf32ea1d410ce9f9d6eb2bd4409d6681902be7
ZIHAD22 <mdzihad2244@gmail.com>
2024-07-03 02:20:58 +0600

    add stack on custome error

--- src/app/error/CError.ts
+++ src/app/error/CError.ts
@@ -1,8 +1,13 @@
 class CError extends Error {
   public statusCode: number;
-  constructor(statusCode: number, message: string) {
+  constructor(statusCode: number, message: string, stack = "") {
     super(message);
     this.statusCode = statusCode;
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
   }
 }
 
