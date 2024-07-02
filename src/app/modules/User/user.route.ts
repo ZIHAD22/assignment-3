ac0093a620116f41db202a2cdcb4248eead6361a
ZIHAD22 <mdzihad2244@gmail.com>
2024-07-03 01:45:35 +0600

    add user validation

--- src/app/modules/User/user.route.ts
+++ src/app/modules/User/user.route.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import { userLogin, userSignUp } from "./user.controller";
 import validateRequest from "../../middlewares/validateRequest";
-import { createUserValidation } from "./user.validation";
+import { createUserValidation, loginUserValidation } from "./user.validation";
 
 const userRouter = Router();
 
 userRouter.post("/signup", validateRequest(createUserValidation), userSignUp);
-userRouter.post("/login", userLogin);
+userRouter.post("/login", validateRequest(loginUserValidation), userLogin);
 
 export default userRouter;
--- src/app/modules/User/user.validation.ts
+++ src/app/modules/User/user.validation.ts
@@ -11,4 +11,11 @@ const createUserValidation = z.object({
   }),
 });
 
-export { createUserValidation };
+const loginUserValidation = z.object({
+  body: z.object({
+    email: z.string({ required_error: "Email is Require" }),
+    password: z.string({ required_error: "Password is Require" }),
+  }),
+});
+
+export { createUserValidation, loginUserValidation };
