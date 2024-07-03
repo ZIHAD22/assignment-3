4e12000e1b1bf0142e9cc3172b8d260c88d1f89e
ZIHAD22 <mdzihad2244@gmail.com>
2024-07-04 01:43:38 +0600

    vercel deployment

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -1,6 +1,7 @@
 import dotEnv from "dotenv";
+import path from "path";
 
-dotEnv.config();
+dotEnv.config({ path: path.join((process.cwd, ".env")) });
 
 // export env variable
 const PORT = process.env.PORT;
@@ -11,11 +12,12 @@ const JWT_ACCESS_SECRET = process.env.JWT_ACCESS_SECRET as string;
 
 // node env variable for database url
 const NODE_ENV = process.env.NODE_ENV;
+
 let prod_url: string;
 
 if (NODE_ENV === "DEV") {
   prod_url = DEV_DB_URL;
-} else if (NODE_ENV === "PROD") {
+} else {
   prod_url = PROD_DB_URL;
 }
 
